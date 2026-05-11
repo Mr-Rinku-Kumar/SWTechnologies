f2530d7cfc02a35f87a3a77d54957d644e6e0fb1
unknown <730551rinku@gmail.com>
2026-05-11 14:33:33 +0530

    add live url after deployed backend on render

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // API Base URL
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://swtechnologies-backend.onrender.com/api';
 
 // Update navbar based on login status
 function updateNavbar() {
